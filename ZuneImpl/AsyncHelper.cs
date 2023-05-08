5d801a71660d70291dfb4bc28de9c7951cf78b85
Yoshi Askharoun <jjask7@gmail.com>
2023-05-07 21:56:04 -0500

    Add hacky async-as-sync helpers

---
+++ ZuneImpl/AsyncHelper.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Diagnostics;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Zune;
+
+public static class AsyncHelper
+{
+    public static void Run(Task task)
+    {
+        if (task == null)
+            return;
+
+        var t = Task.Run(() => task);
+        t.Wait();
+    }
+
+    public static TResult Run<TResult>(Func<Task<TResult>> action)
+    {
+        Guard.IsNotNull(action);
+
+        var t = Task.Run(action);
+        t.Wait();
+        return t.Result;
+    }
+}
--- ZuneImpl/Extensions.cs
+++ ZuneImpl/Extensions.cs
@@ -1,31 +1,27 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
-
-#if NETSTANDARD1_0_OR_GREATER
 using System.Net.Http;
 using System.Threading.Tasks;
-#endif
 
-namespace Microsoft.Zune
+namespace Microsoft.Zune;
+
+internal static class Extensions
 {
-    internal static class Extensions
-    {
 #if NETSTANDARD2_0_OR_GREATER && !NET5_0_OR_GREATER
-        public static Stream ReadAsStream(this HttpContent content)
-        {
-            return Task.Run(content.ReadAsStreamAsync).Result;
-        }
-
-        public static HttpResponseMessage Send(this HttpClient client, HttpRequestMessage request)
-        {
-            return Task.Run(() => client.SendAsync(request)).Result;
-        }
-
-        public static Task<Stream> GetStreamAsync(this HttpClient client, Uri uri, CancellationToken _)
-        {
-            return client.GetStreamAsync(uri);
-        }
-#endif
+    public static Stream ReadAsStream(this HttpContent content)
+    {
+        return AsyncHelper.Run(content.ReadAsStreamAsync);
     }
+
+    public static HttpResponseMessage Send(this HttpClient client, HttpRequestMessage request)
+    {
+        return AsyncHelper.Run(() => client.SendAsync(request));
+    }
+
+    public static Task<Stream> GetStreamAsync(this HttpClient client, Uri uri, CancellationToken _)
+    {
+        return client.GetStreamAsync(uri);
+    }
+#endif
 }
