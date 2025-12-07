cdd0f753534c09d11a2f1d5459a0e4995b394927
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-12-07 02:03:43 -0600

    Allow CComPtrMgd to compile

--- ZuneDBApi/Interop/CComPtrMgd.cs
+++ ZuneDBApi/Interop/CComPtrMgd.cs
@@ -4,47 +4,25 @@ using System.Runtime.InteropServices;
 
 namespace ZuneDBApi.Interop;
 
-internal class CComPtrMgd<T> : IDisposable where T : unmanaged
+internal unsafe class CComPtrMgd<T> : IDisposable where T : unmanaged
 {
-    public unsafe T* p = null;
+    public T* p = null;
 
-    private void _007ECComPtrMgd()
+    public void Release()
     {
-        Release();
-    }
-
-    private void _0021CComPtrMgd()
-    {
-        Release();
-    }
-
-    public unsafe void Release()
-    {
-        //IL_0014: Expected I, but got I8
-        //IL_0021: Expected I, but got I8
-        T* ptr = p;
-        T* ptr2 = ptr;
+        var ptr = p;
         if (ptr != null)
         {
             p = null;
-            ((delegate* unmanaged[Cdecl, Cdecl]<IntPtr, uint>)(*(ulong*)(*(long*)ptr2 + 16)))((nint)ptr2);
+            ((delegate* unmanaged[Cdecl, Cdecl]<IntPtr, uint>)(*(ulong*)(*(long*)ptr + 16)))((nint)ptr);
         }
     }
 
-    protected virtual void Dispose([MarshalAs(UnmanagedType.U1)] bool P_0)
+    protected virtual void Dispose([MarshalAs(UnmanagedType.U1)] bool disposing)
     {
-        if (P_0)
+        if (disposing)
         {
             Release();
-            return;
-        }
-        try
-        {
-            Release();
-        }
-        finally
-        {
-            base.Finalize();
         }
     }
 
@@ -59,21 +37,21 @@ internal class CComPtrMgd<T> : IDisposable where T : unmanaged
         Dispose(false);
     }
 
-    public static unsafe implicit operator T*(CComPtrMgd<T> comPtr)
+    public static implicit operator T*(CComPtrMgd<T> comPtr)
     {
         return comPtr.p;
     }
 
     [SpecialName]
-    public unsafe T* op_MemberSelection()
+    public static T* op_MemberSelection(CComPtrMgd<T> comPtr)
     {
-        return p;
+        // TODO: C# doesn't respect these operator overloads
+        return comPtr.p;
     }
 
     [SpecialName]
-    public unsafe T* op_Assign(T* lp)
+    public T* op_Assign(T* lp)
     {
-        //IL_001c: Expected I, but got I8
         Release();
         p = lp;
         if (lp != null)
@@ -84,8 +62,8 @@ internal class CComPtrMgd<T> : IDisposable where T : unmanaged
     }
 
     [return: MarshalAs(UnmanagedType.U1)]
-    public unsafe bool operator !()
+    public static unsafe bool operator !(CComPtrMgd<T> comPtr)
     {
-        return (long)(nint)p == 0;
+        return (nint)comPtr.p == 0;
     }
 }
