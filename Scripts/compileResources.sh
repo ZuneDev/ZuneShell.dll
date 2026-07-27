9d9a6acfd964d929268cf619e4a1cf77a0d42075
Yoshi Askharoun <jjask7@gmail.com>
2026-07-27 11:01:49 -0500

    Add resource compilation for ZuneMarketplaceResources and update script to include UIXcontrols

--- Scripts/compileResources.sh
+++ Scripts/compileResources.sh
@@ -6,11 +6,12 @@ echo "Built UIXC, compiling resources..."
 declare -a RSRCS=(
 	"../ZuneShell/Resources/RCDATA ../ZuneShell/Resources/RCDATA.resx"
 	"../ZuneShell/Resources/RCDATA/Marketplace ../ZuneShell/Resources/RCDATA.Marketplace.resx"
+	"../libs/ZuneUIXTools/libs/MicrosoftIris/UIXcontrols/Resources/RCDATA ../libs/ZuneUIXTools/libs/MicrosoftIris/UIXcontrols/Resources/RCDATA.resx"
 )
-
 
 for rsrc in "${RSRCS[@]}"; do
     read -a strarr <<< "$rsrc"  # uses default whitespace IFS
-    dotnet run --project $UIXC_CSPROJ --no-build -- resx -i ${strarr[0]} -o ${strarr[1]}
+    dotnet run --project $UIXC_CSPROJ --no-build --no-launch-profile \
+		-- resx -i ${strarr[0]} -o ${strarr[1]}
 done
 
